2645bf148f023e192e5609a7b8862402f872de8a
Bob Christenson <bob@mustardseedmedia.com>
2017-08-21 15:10:26 -0400

    add new favicons

--- sites/all/themes/fos/templates/page/html.tpl.php
+++ sites/all/themes/fos/templates/page/html.tpl.php
@@ -12,23 +12,23 @@
   <script>var googletag = googletag || {}; googletag.cmd = googletag.cmd || [];</script>
   <?php print $scripts; ?>
 
-  <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/sites/all/themes/fos/favicons/apple-icon-180x180.png">
-  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/sites/all/themes/fos/favicons/apple-icon-152x152.png">
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/sites/all/themes/fos/favicons/apple-icon-144x144.png">
-  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/sites/all/themes/fos/favicons/apple-icon-120x120.png">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/sites/all/themes/fos/favicons/apple-icon-114x114.png">
-  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/sites/all/themes/fos/favicons/apple-icon-76x76.png">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/sites/all/themes/fos/favicons/apple-icon-72x72.png">
-  <link rel="apple-touch-icon-precomposed" href="/sites/all/themes/fos/favicons/apple-icon.png">
-  <link rel="icon" type="image/x-icon" sizes="192x192"  href="/sites/all/themes/fos/favicons/icon-192x192.png">
-  <link rel="icon" type="image/x-icon" sizes="32x32" href="/sites/all/themes/fos/favicons/icon-32x32.png">
-  <link rel="icon" type="image/x-icon" sizes="96x96" href="/sites/all/themes/fos/favicons/icon-96x96.png">
-  <link rel="icon" type="image/x-icon" sizes="16x16" href="/sites/all/themes/fos/favicons/icon-16x16.png">
-  <link rel="manifest" href="/sites/all/themes/fos/favicons//manifest.json">
-  <meta name="msapplication-config" content="/sites/all/themes/fos/favicons/browserconfig.xml" />
-  <meta name="msapplication-TileColor" content="#f58b2c">
-  <meta name="msapplication-TileImage" content="/sites/all/themes/fos/favicons/apple-icon-144x144.png">
-  <meta name="theme-color" content="#f58b2c">
+	<link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/fos/favicons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/sites/all/themes/fos/favicons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/fos/favicons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/sites/all/themes/fos/favicons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/sites/all/themes/fos/favicons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/sites/all/themes/fos/favicons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/sites/all/themes/fos/favicons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/sites/all/themes/fos/favicons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/sites/all/themes/fos/favicons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/sites/all/themes/fos/favicons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/sites/all/themes/fos/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/sites/all/themes/fos/favicons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/sites/all/themes/fos/favicons/favicon-16x16.png">
+	<link rel="manifest" href="/sites/all/themes/fos/favicons/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 
   <!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
