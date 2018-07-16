0d90a0199023185ddff527857203c5a95b7a7bf9
Sojo Server <bsabath@sojo.net>
2018-07-16 11:09:30 -0400

    updated core

--- sites/all/modules/contrib/auto_nodetitle/auto_nodetitle.js
+++ sites/all/modules/contrib/auto_nodetitle/auto_nodetitle.js
@@ -1,19 +1,23 @@
+/**
+ * @file
+ */
+
 (function ($) {
 
 Drupal.behaviors.auto_nodetitleFieldsetSummaries = {
   attach: function (context) {
     $('fieldset#edit-auto-nodetitle', context).drupalSetSummary(function (context) {
 
-      // Retrieve the value of the selected radio button
-      var ant = $("input[@name=#edit-auto-nodetitle-ant]:checked").val();
+      // Retrieve the value of the selected radio button.
+      var ant = $("input[name='ant']:checked").val();
 
-      if (ant==0) {
+      if (ant == 0) {
         return Drupal.t('Disabled')
       }
-      else if (ant==1) {
+      else if (ant == 1) {
         return Drupal.t('Automatic (hide title field)')
       }
-      else if (ant==2) {
+      else if (ant == 2) {
         return Drupal.t('Automatic (if title empty)')
       }
     });
